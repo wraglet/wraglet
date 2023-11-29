af7739ab7e67a6df8a01a7e7b5fc18fda36f5a69
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2023-11-29 19:02:08 +0800

    Try to just render the AblyProvider without the FeedBody

--- src/app/feed/components/FeedBody/component/FeedAblyProvider.tsx
+++ src/app/feed/components/FeedBody/component/FeedAblyProvider.tsx
@@ -10,14 +10,17 @@ const client = new Ably.Realtime.Promise({
   authMethod: 'POST'
 });
 
-type Props = {
+const FeedAblyProvider = ({
+  initialPosts
+}: {
   initialPosts: PostInterface[];
-};
-
-const FeedAblyProvider = ({ initialPosts }: Props) => {
+}) => {
   return (
     <AblyProvider client={client}>
-      <FeedBody initialPosts={initialPosts} />
+      {/* <FeedBody initialPosts={initialPosts} /> */}
+      <section className='col-start-3 col-end-8 h-auto flex flex-col mt-6 w-full gap-y-4 overflow-auto'>
+        <p>This is a wrapped AblyProvider</p>
+      </section>
     </AblyProvider>
   );
 };
