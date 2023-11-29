e4d27efe41a748593567b62c46c27c89ee1139b4
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2023-11-29 19:11:31 +0800

    Insert ably client inside FeedAblyProvider

--- src/app/feed/components/FeedBody/component/FeedAblyProvider.tsx
+++ src/app/feed/components/FeedBody/component/FeedAblyProvider.tsx
@@ -5,16 +5,15 @@ import * as Ably from 'ably';
 import { AblyProvider } from 'ably/react';
 import FeedBody from '..';
 
-const client = new Ably.Realtime.Promise({
-  authUrl: '/api/token',
-  authMethod: 'POST'
-});
-
 const FeedAblyProvider = ({
   initialPosts
 }: {
   initialPosts: PostInterface[];
 }) => {
+  const client = new Ably.Realtime.Promise({
+    authUrl: '/api/token',
+    authMethod: 'POST'
+  });
   return (
     <AblyProvider client={client}>
       {/* <FeedBody initialPosts={initialPosts} /> */}
