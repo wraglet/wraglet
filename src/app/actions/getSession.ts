978403fc41a0f4b4e1aeb72076184e8fbfeb14d1
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2023-11-30 10:47:19 +0800

    Add error handling on every async await functions to catch all possible errors

--- src/app/actions/getSession.ts
+++ src/app/actions/getSession.ts
@@ -2,5 +2,10 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../utils/authOptions';
 
 export default async function GetSession() {
-  return await getServerSession(authOptions);
+  return await getServerSession(authOptions).catch((err) => {
+    console.error(
+      'Error happened while getting getServerSession(authOptions) at getSession.ts: ',
+      err
+    );
+  });
 }
