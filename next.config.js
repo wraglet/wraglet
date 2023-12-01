087070f8ecd4429b858ef0dcbfbcaebb043b0010
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2023-12-02 00:08:35 +0800

    swcMinify: false

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // disabling strict mode since the examples use
-  // useEffect with no dependencies to ensure the function
-  // is only run once and only run on the client.
-  reactStrictMode: false,
-  swcMinify: true,
-  webpack: (config) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      bufferutil: 'commonjs bufferutil'
-    });
-    return config;
-  }
+  swcMinify: false
 };
 
 module.exports = nextConfig;
