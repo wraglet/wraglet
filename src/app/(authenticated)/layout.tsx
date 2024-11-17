993cae2c5a815234e98dd9e4a283ae87b6ee264b
jerald-devOfficial <spaueOfficial@gmail.com>
2024-11-18 05:01:27 +0800

    feat: add session checker in authenticated layout to redicret to / if no session found

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,4 +1,6 @@
+import { redirect } from 'next/navigation'
 import getCurrentUser from '@/actions/getCurrentUser'
+import getSession from '@/actions/getSession'
 import deJSONify from '@/utils/deJSONify'
 
 import Header from '@/components/Header'
@@ -10,21 +12,20 @@ const AuthenticatedLayout = async ({
 }: {
   children: React.ReactNode
 }) => {
-  const jsonUser = await getCurrentUser().catch((err) => {
-    console.error(
-      'Error happened while getting getCurrentUser() on AuthenticatedLayout component: ',
-      err
-    )
-  })
+  const session = await getSession()
+  const jsonUser = await getCurrentUser()
 
   const currentUser = deJSONify(jsonUser)
 
-  return (
-    <div className="relative flex min-h-screen flex-col items-center overflow-hidden bg-[rgba(110,201,247,0.15)]">
-      <Header currentUser={currentUser} />
-      {children}
-    </div>
-  )
+  if (!session) {
+    redirect('/')
+  } else
+    return (
+      <div className="relative flex min-h-screen flex-col items-center overflow-hidden bg-[rgba(110,201,247,0.15)]">
+        <Header currentUser={currentUser} />
+        {children}
+      </div>
+    )
 }
 
 export default AuthenticatedLayout
