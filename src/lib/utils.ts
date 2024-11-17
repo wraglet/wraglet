8105ad5cf82eef7e4004b76716a0d21711a844ad
jerald-devOfficial <spaueOfficial@gmail.com>
2024-11-18 03:49:49 +0800

    chore: delete generateUsernames.ts file, convert utils functions to arrow functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
-export function generateUsername(firstName: string, lastName: string): string {
+export const generateUsername = (firstName: string, lastName: string): string => {
   const firstNameWithoutSpaces = firstName.toLowerCase().replace(/\s/g, '')
   const lastNameWithoutSpaces = lastName.toLowerCase().replace(/\s/g, '')
   const randomDigits = Math.floor(Math.random() * 90) + 10 // Generate random two-digit number
