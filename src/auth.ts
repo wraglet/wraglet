d94d09089d6b301a79d332219b964a500197cd3e
jerald-devOfficial <spaueOfficial@gmail.com>
2024-11-18 03:39:37 +0800

    feat: delete middleware.ts, authOptions.ts, AuthContext.ts, replace with auth.ts and update [...nextauth] route based on latest auth.js docs

---
+++ src/auth.ts
@@ -0,0 +1,65 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import client from '@/lib/db'
+import User from '@/models/User'
+import bcrypt from 'bcryptjs'
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+        try {
+          await client()
+          console.log('Connected to MongoDB')
+
+          const user = await User.findOne({
+            email: (credentials?.email as string).toLowerCase()
+          }).lean()
+
+          if (user) {
+            console.log('User found:', user)
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password as string,
+              user.hashedPassword
+            )
+            console.log('Password comparison result:', isPasswordCorrect)
+
+            if (isPasswordCorrect) {
+              return {
+                id: user._id.toString(),
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                username: user.username
+              }
+            }
+          } else {
+            console.log('User not found with email:', credentials?.email)
+          }
+        } catch (error) {
+          console.error('Error during authorization:', error)
+        }
+        return null
+      }
+    })
+  ],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth
+    }
+  },
+  debug: process.env.NODE_ENV === 'development',
+  session: {
+    strategy: 'jwt'
+  },
+  secret: process.env.AUTH_SECRET,
+  pages: {
+    signIn: '/',
+    error: '/'
+  }
+})
