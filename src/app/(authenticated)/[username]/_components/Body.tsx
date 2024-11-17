0e6dc8b59dfd2f4b9716f8a0aa4a6317a97facda
jerald-devOfficial <spaueOfficial@gmail.com>
2024-11-18 05:06:32 +0800

    feat: replace posts state and setter in [username] Body component with useQuery and update the setUserPosts on query, also useQuery for user

--- src/app/(authenticated)/[username]/_components/Body.tsx
+++ src/app/(authenticated)/[username]/_components/Body.tsx
@@ -1,9 +1,17 @@
 'use client'
 
-import React, { FormEvent, useReducer, useState } from 'react'
-import { PostInterface } from '@/interfaces'
+import React, {
+  FormEvent,
+  Suspense,
+  useEffect,
+  useReducer,
+  useState
+} from 'react'
+import getPostsByUsername from '@/actions/getPostsByUsername'
+import getUserByUsername from '@/actions/getUserByUsername'
 import { PostDocument } from '@/models/Post'
 import useFeedPostsStore from '@/store/feedPosts'
+import { useQuery } from '@tanstack/react-query'
 import { useChannel } from 'ably/react'
 import axios from 'axios'
 import toast from 'react-hot-toast'
@@ -11,18 +19,37 @@ import toast from 'react-hot-toast'
 import CreatePost from '@/components/CreatePost'
 import Post from '@/components/Post'
 
-type Props = {
+type ProfileBodyProps = {
+  username: string
   initialPosts: PostDocument[]
 }
 
-const ProfileBody = ({ initialPosts }: Props) => {
+const ProfileBody = ({ username, initialPosts }: ProfileBodyProps) => {
+  const [hydrated, setHydrated] = useState(false)
   const reducer = (state: any, action: any) => ({ ...state, ...action })
 
+  const [userPosts, setUserPosts] = useState(initialPosts)
+
+  // this is used to update the feed posts
   const { posts: feedPosts, setFeedPosts } = useFeedPostsStore()
 
-  const [posts, setPosts] = useState<PostInterface[]>(
-    initialPosts as unknown as PostInterface[]
-  )
+  const { data: user } = useQuery({
+    queryKey: ['user', username],
+    queryFn: async () => await getUserByUsername(username)
+  })
+
+  const { isPending } = useQuery({
+    queryKey: ['posts', username],
+    queryFn: async () => {
+      const posts = await getPostsByUsername(username)
+      setUserPosts(posts)
+      return posts
+    },
+    initialData: initialPosts,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false
+  })
 
   const initialState = {
     text: '',
@@ -51,27 +78,43 @@ const ProfileBody = ({ initialPosts }: Props) => {
         })
 
         setFeedPosts([res.data, ...feedPosts])
-
-        setPosts((posts: PostInterface[]) => [res.data, ...posts])
+        setUserPosts([res.data, ...userPosts])
       })
       .catch(() => toast.error('An error occurred when creating a post'))
       .finally(() => dispatchState({ isLoading: false, text: '', image: null }))
   }
+
+  useEffect(() => {
+    setHydrated(true)
+  }, [])
+
+  if (!hydrated) return null
+
   return (
     <div className="mb-6 flex w-full items-start gap-x-10 tablet:px-5 lg:px-10 xl:w-[1250px] xl:px-0">
       <div className="hidden h-[500px] flex-col rounded-lg border border-solid border-neutral-200 bg-white drop-shadow-md tablet:flex tablet:w-2/5" />
       <div className="flex w-full flex-col gap-y-4 sm:mx-10 md:mx-auto md:w-[680px] tablet:flex-grow">
-        <CreatePost
-          isLoading={isLoading}
-          submitPost={submitPost}
-          text={text}
-          setText={(e) => dispatchState({ text: e.target.value })}
-          postImage={image}
-          setPostImage={(image) => dispatchState({ image: image })}
-        />
-        {posts.map((post: PostInterface) => (
-          <Post key={post._id} post={post} />
-        ))}
+        {user?.isCurrentUser && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <CreatePost
+              isLoading={isLoading}
+              submitPost={submitPost}
+              text={text}
+              setText={(e) => dispatchState({ text: e.target.value })}
+              postImage={image}
+              setPostImage={(image) => dispatchState({ image: image })}
+            />
+          </Suspense>
+        )}
+        {isPending && <div>Loading...</div>}
+        {userPosts &&
+          !isPending &&
+          userPosts.map((post: PostDocument) => (
+            <Post
+              key={post._id as string}
+              post={post as unknown as PostDocument}
+            />
+          ))}
       </div>
     </div>
   )
