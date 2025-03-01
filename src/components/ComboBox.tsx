2f70aef27d447565c3466a8863aa423d22a3d035
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-03-02 00:16:25 +0800

    chore: update ComboBox and ListBox components from headlessui

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -1,17 +1,24 @@
 import {
   forwardRef,
   ForwardRefRenderFunction,
-  InputHTMLAttributes,
-  Fragment
-} from 'react';
-import { Combobox, Transition } from '@headlessui/react';
-import { HiChevronDown } from 'react-icons/hi2';
+  Fragment,
+  InputHTMLAttributes
+} from 'react'
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+  Transition
+} from '@headlessui/react'
+import { HiChevronDown } from 'react-icons/hi2'
 
 interface ComboBoxProps extends InputHTMLAttributes<HTMLInputElement> {
-  options: string[];
-  label?: string;
-  onChange: (e: any) => void;
-  value: string;
+  options: string[]
+  label?: string
+  onChange: (e: any) => void
+  value: string
 }
 
 const ComboBox: ForwardRefRenderFunction<HTMLInputElement, ComboBoxProps> = (
@@ -20,42 +27,42 @@ const ComboBox: ForwardRefRenderFunction<HTMLInputElement, ComboBoxProps> = (
 ) => {
   const filteredOptions = options.filter((option) =>
     option.toLowerCase().includes(value.toLowerCase())
-  );
+  )
 
   return (
-    <div className='flex flex-col mb-4 w-full'>
-      {label && <label className='mb-1 text-sm'>{label}</label>}
-      <Combobox as='div' className='relative z-50'>
-        <div className='relative w-full cursor-default overflow-hidden'>
-          <Combobox.Input
+    <div className="mb-4 flex w-full flex-col">
+      {label && <label className="mb-1 text-sm">{label}</label>}
+      <Combobox as="div" className="relative z-50">
+        <div className="relative w-full cursor-default overflow-hidden">
+          <ComboboxInput
             ref={ref}
             {...props}
-            className='hover:border-slate-300 focus:border-indigo-300 h-8 w-full border border-solid border-slate-200 rounded py-1.5 px-4 text-sm active:border-indigo-300 focus:outline-none'
+            className="h-8 w-full rounded border border-solid border-slate-200 px-4 py-1.5 text-sm hover:border-slate-300 focus:border-indigo-300 focus:outline-none active:border-indigo-300"
             onChange={(e) => onChange(e)}
             value={value}
           />
-          <Combobox.Button className='absolute inset-y-0 right-0 flex items-center pr-2'>
+          <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2">
             <HiChevronDown
-              className='h-5 w-5 text-gray-400'
-              aria-hidden='true'
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
             />
-          </Combobox.Button>
+          </ComboboxButton>
         </div>
         <Transition
           as={Fragment}
-          leave='transition ease-in duration-100'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          <Combobox.Options
-            as='ul'
-            className='absolute max-h-60 w-full py-2 bg-white border border-solid overflow-y-auto border-slate-200 rounded shadow-lg text-sm'
+          <ComboboxOptions
+            as="ul"
+            className="absolute max-h-60 w-full overflow-y-auto rounded border border-solid border-slate-200 bg-white py-2 text-sm shadow-lg"
           >
             {filteredOptions.length === 0 && value !== '' ? (
-              <li className='px-4 py-2 text-gray-700'>Nothing found.</li>
+              <li className="px-4 py-2 text-gray-700">Nothing found.</li>
             ) : (
               filteredOptions.map((option) => (
-                <Combobox.Option key={option} value={option}>
+                <ComboboxOption key={option} value={option}>
                   {({ active, selected }) => (
                     <li
                       className={`cursor-pointer px-4 py-2 ${
@@ -65,16 +72,16 @@ const ComboBox: ForwardRefRenderFunction<HTMLInputElement, ComboBoxProps> = (
                       {option}
                     </li>
                   )}
-                </Combobox.Option>
+                </ComboboxOption>
               ))
             )}
-          </Combobox.Options>
+          </ComboboxOptions>
         </Transition>
       </Combobox>
     </div>
-  );
-};
+  )
+}
 
-ComboBox.displayName = 'ComboBox';
+ComboBox.displayName = 'ComboBox'
 
-export default forwardRef(ComboBox);
+export default forwardRef(ComboBox)
--- src/components/ListBox.tsx
+++ src/components/ListBox.tsx
@@ -1,57 +1,63 @@
-import { ForwardRefRenderFunction, Fragment, InputHTMLAttributes } from 'react';
-import { Listbox, Transition } from '@headlessui/react';
-import { HiCheck, HiChevronUpDown } from 'react-icons/hi2';
+import { FC, Fragment, InputHTMLAttributes } from 'react'
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+  Transition
+} from '@headlessui/react'
+import { HiCheck, HiChevronUpDown } from 'react-icons/hi2'
 
 export type ListProps = {
-  val: string;
-  name: string;
-};
+  val: string
+  name: string
+}
 
 interface ListBoxProps extends InputHTMLAttributes<HTMLInputElement> {
   options:
     | {
-        val: string;
-        name: string;
+        val: string
+        name: string
       }[]
-    | string[];
-  setSelected: (val: string | ListProps) => void;
-  selected: string | ListProps;
-  label?: string;
+    | string[]
+  setSelected: (val: string | ListProps) => void
+  selected: string | ListProps
+  label?: string
 }
 
-const ListBox: ForwardRefRenderFunction<HTMLInputElement, ListBoxProps> = ({
+const ListBox: FC<ListBoxProps> = ({
   options,
   selected,
   setSelected,
   label
 }) => {
   return (
-    <div className='flex flex-col w-full'>
+    <div className="flex w-full flex-col">
       {label && (
-        <label className='block text-sm text-slate-600 mb-1'>{label}</label>
+        <label className="mb-1 block text-sm text-slate-600">{label}</label>
       )}
       <Listbox value={selected} onChange={(val) => setSelected(val)}>
-        <div className='relative'>
-          <Listbox.Button className='relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm'>
-            <span className='block truncate'>
+        <div className="relative">
+          <ListboxButton className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">
               {typeof selected === 'object' ? selected.name : selected}
             </span>
-            <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <HiChevronUpDown
-                className='h-5 w-5 text-gray-400'
-                aria-hidden='true'
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
               />
             </span>
-          </Listbox.Button>
+          </ListboxButton>
           <Transition
             as={Fragment}
-            leave='transition ease-in duration-100'
-            leaveFrom='opacity-100'
-            leaveTo='opacity-0'
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            <Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm z-30'>
+            <ListboxOptions className="absolute z-30 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
               {options.map((option, i) => (
-                <Listbox.Option
+                <ListboxOption
                   key={i}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
@@ -70,20 +76,20 @@ const ListBox: ForwardRefRenderFunction<HTMLInputElement, ListBoxProps> = ({
                         {typeof option === 'object' ? option.name : option}
                       </span>
                       {selected ? (
-                        <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600'>
-                          <HiCheck className='h-5 w-5' aria-hidden='true' />
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          <HiCheck className="h-5 w-5" aria-hidden="true" />
                         </span>
                       ) : null}
                     </>
                   )}
-                </Listbox.Option>
+                </ListboxOption>
               ))}
-            </Listbox.Options>
+            </ListboxOptions>
           </Transition>
         </div>
       </Listbox>
     </div>
-  );
-};
+  )
+}
 
-export default ListBox;
+export default ListBox
