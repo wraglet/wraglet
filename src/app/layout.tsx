b1ee7d0e01158525219aeed389fdcb8b3b39a661
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2025-05-26 15:47:20 +0800

    feat: integrate Vercel Speed Insights for performance monitoring in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import '@/app/globals.css'
 
@@ -60,6 +61,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
           <ToasterContext />
           {children}
           <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
